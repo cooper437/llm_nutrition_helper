79a9aa1f08eded61ef8a672e52a89c9a3e631df4
Sam Cooper <cooper437@gmail.com>
2023-10-15 14:07:40 -0400

    WIP

--- src/foundation_food_vector_store.py
+++ src/foundation_food_vector_store.py
@@ -2,42 +2,50 @@ import chromadb
 import logging
 from src.foundation_food_parser import parse_foundation_ingredients_nutrition_df
 
-chroma_client = None
-collection = None
-if not chroma_client:
-    chroma_client = chromadb.Client()
+# chroma_client = None
+# collection = None
+# if not chroma_client:
+#     chroma_client = chromadb.Client()
 
-if not collection:
-    collection = chroma_client.create_collection(name="foundation_foods")
-
-
-def build_vector_store_index():
-    logging.info("Parsing foundation foods data into a dataframe")
-    foundation_foods_df = parse_foundation_ingredients_nutrition_df()
-    logging.info("Building vector store index")
-    collection.add(
-        documents=list(foundation_foods_df["description"]),
-        metadatas=foundation_foods_df.to_dict(orient="records"),
-        ids=list(foundation_foods_df["fdcId"]),
-    )
+# if not collection:
+#     collection = chroma_client.create_collection(name="foundation_foods")
 
 
-def query_vector_store_index(an_ingredient: str, n_results: int = 1):
-    print(f"Querying vector store index for {an_ingredient}")
-    results = collection.query(query_texts=[an_ingredient], n_results=n_results)
-    print(f"Most similar result: {results}")
-    description = results["documents"][0][0]
-    calories = results["metadatas"][0][0]["calories"]
-    carbs = results["metadatas"][0][0]["carbs"]
-    fat = results["metadatas"][0][0]["fat"]
-    protein = results["metadatas"][0][0]["protein"]
-    return {
-        "Description": description,
-        "Calories (kcal)": calories,
-        "Carbs (g)": carbs,
-        "Fat (g)": fat,
-        "Protein (g)": protein,
-    }
+class FoundationFoodsVectorStore:
+    def __init__(self):
+        self.chroma_client = None
+        self.collection = None
 
+    def initialize_vector_db(self):
+        print("Initializing vector db")
+        self.chroma_client = chromadb.Client()
+        self.collection = self.chroma_client.create_collection(name="foundation_foods")
 
-build_vector_store_index()
+    def build_vector_store_index(self):
+        logging.info("Parsing foundation foods data into a dataframe")
+        foundation_foods_df = parse_foundation_ingredients_nutrition_df()
+        logging.info("Building vector store index")
+        self.collection.add(
+            documents=list(foundation_foods_df["description"]),
+            metadatas=foundation_foods_df.to_dict(orient="records"),
+            ids=list(foundation_foods_df["fdcId"]),
+        )
+
+    def query_vector_store_index(self, an_ingredient: str, n_results: int = 1):
+        print(f"Querying vector store index for {an_ingredient}")
+        results = self.collection.query(
+            query_texts=[an_ingredient], n_results=n_results
+        )
+        print(f"Most similar result: {results}")
+        description = results["documents"][0][0]
+        calories = results["metadatas"][0][0]["calories"]
+        carbs = results["metadatas"][0][0]["carbs"]
+        fat = results["metadatas"][0][0]["fat"]
+        protein = results["metadatas"][0][0]["protein"]
+        return {
+            "Description": description,
+            "Calories (kcal)": calories,
+            "Carbs (g)": carbs,
+            "Fat (g)": fat,
+            "Protein (g)": protein,
+        }
--- src/main.py
+++ src/main.py
@@ -1,18 +1,13 @@
 import streamlit as st
 import pandas as pd
-from llama_index import VectorStoreIndex, ServiceContext, Document
-from llama_index.llms import OpenAI
 import openai
-from llama_index import SimpleDirectoryReader
 from src.meal_name_expander import generate_meal_names
 from src.common_ingredient_finder import (
     generate_prominent_ingredients,
     parse_json_string,
 )
-from src.foundation_food_vector_store import query_vector_store_index
-from llama_index.query_engine.pandas_query_engine import PandasQueryEngine
+from src.foundation_food_vector_store import FoundationFoodsVectorStore
 
-# openai.log = "debug"
 
 st.set_page_config(
     page_title="Look up foods based on nutritional needs",
@@ -22,12 +17,19 @@ st.set_page_config(
     menu_items=None,
 )
 openai.api_key = st.secrets.openai_key
-st.title("Chat with the Streamlit docs, powered by LlamaIndex 💬🦙")
-st.info(
-    "Check out the full tutorial to build this app in our [blog post](https://blog.streamlit.io/build-a-chatbot-with-custom-data-sources-powered-by-llamaindex/)",
-    icon="📃",
-)
-prompt = st.chat_input("Say something")
+st.title("Meal Nutrition Helper")
+# prompt = st.chat_input("Say something")
+
+
+@st.cache_resource(show_spinner=True)
+def init_db():
+    vector_db = FoundationFoodsVectorStore()
+    vector_db.initialize_vector_db()
+    vector_db.build_vector_store_index()
+    return vector_db
+
+
+vector_db = init_db()
 
 if "messages" not in st.session_state.keys():  # Initialize the chat messages history
     st.session_state.messages = [
@@ -38,13 +40,13 @@ if "messages" not in st.session_state.keys():  # Initialize the chat messages hi
     ]
 
 if prompt := st.chat_input(
-    "Description of a meal or snack"
+    "Short description of a meal or snack"
 ):  # Prompt for user input and save to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
 
-for first_message in st.session_state.messages:  # Display the prior chat messages
-    with st.chat_message(first_message["role"]):
-        st.write(first_message["content"])
+for a_message in st.session_state.messages:  # Display the prior chat messages
+    with st.chat_message(a_message["role"]):
+        st.write(a_message["content"])
 
 if st.session_state.messages[-1]["role"] != "assistant":
     with st.chat_message("assistant"):
@@ -69,7 +71,7 @@ if st.session_state.messages[-1]["role"] != "assistant":
             st.write("Finding nutrition info for these ingredients...")
             foundation_ingredients_df = []
             for an_ingredient in prominent_ingredients_list:
-                most_similar_foundation_ingredient = query_vector_store_index(
+                most_similar_foundation_ingredient = vector_db.query_vector_store_index(
                     an_ingredient
                 )
                 foundation_ingredients_df.append(most_similar_foundation_ingredient)
@@ -86,4 +88,9 @@ if st.session_state.messages[-1]["role"] != "assistant":
                 "And here's the total estimated nutrition info for your whole meal:"
             )
             st.dataframe(nutrition_sum_df, column_config={"0": "Totals"})
-            # st.session_state.messages.append(first_message)  # Add response to message history
+    next_message = {
+        "role": "assistant",
+        "content": "Thanks for using the Meal Nutrition Helper! Try again?",
+    }
+    with st.chat_message(next_message["role"]):
+        st.write(next_message["content"])
